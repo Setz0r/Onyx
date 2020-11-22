4b656ca477700252e335ea950876ea8b98e4216a
Set Zor <setzor@gmail.com>
2020-11-22 05:39:38 -0600

    Update logging, add zeroMQ to networking library, added conf support

---
+++ Data/Game/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using Data.World;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Toolbelt;
+
+namespace Data.Game
+{
+    public class GameManager
+    {
+        public Zone[] Zones;
+
+        public void Initialize()
+        {            
+            Logger.Info("Initializing Game Manager");
+            // @todo: initialize here
+            Logger.Info("Game Manager Initialized, Ready to Rock!");
+        }
+
+        public void GameLoop()
+        {
+            bool active = true;
+            Logger.Info("Entering Game Loop");
+            while(active)
+            {
+                if (Console.KeyAvailable)
+                {
+                    active = false;
+                    Logger.Info("Leaving Game Loop");
+                }
+                Thread.Sleep(1); // Prevent CPU overload
+            }
+        }
+
+        public void Shutdown()
+        {
+            Logger.Info("Shutting Down Game Manager");
+        }
+
+    }
+}
--- ZoneServer/Program.cs
+++ ZoneServer/Program.cs
@@ -8,95 +8,143 @@ using System.Runtime.InteropServices;
 using Data.DataChunks.Outgoing;
 using Data.Game;
 using System.Threading;
+using System.IO;
+using Toolbelt;
+using System.Collections.Generic;
+using static Toolbelt.Logger;
 
 namespace ZoneServer
 {
     class Program
     {
-        [StructLayout(LayoutKind.Explicit)]
-        public struct TestData
+        public const string ConfigurationFile = "conf/game.conf";
+        public static Dictionary<string,string> Configuration;
+
+        static bool LoadConfiguration()
         {
-            [FieldOffset(0)]
-            public float x;
-            [FieldOffset(4)]
-            public float y;
-            [FieldOffset(8)]
-            public float z;
+            Logger.SetLoggingLevel(LOGGINGLEVEL.ERROR);
+            try
+            {
+                if (File.Exists(ConfigurationFile))
+                {
+                    Configuration = Utility.ReadConf(ConfigurationFile);
+                }
+                else
+                {
+                    Logger.Warning("Zone server configuration file missing");
+                }
+                foreach (var config in Configuration)
+                {
+                    switch (config.Key)
+                    {
+                        case "Logging":
+                            LOGGINGLEVEL logLevel = (LOGGINGLEVEL)Convert.ToUInt16(config.Value);
+                            Logger.SetLoggingLevel(logLevel);
+                            break;
+                    }
+                }
+            } 
+            catch (Exception e)
+            {
+                Logger.Error("LoadingConfiguration : {0}", new[] { e.Message });
+                return false;
+            }
+            return true;
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.GetMaxThreads(out int workerThreadsCount, out int ioThreadsCount);
+            if (LoadConfiguration())
+            {
+                TestJunk();
+                GameManager game = new GameManager();
+                game.Initialize();
+                game.GameLoop();
+                game.Shutdown();                
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
 
-            byte[] responseData = Toolbelt.Utility.StringToByteArray("4c000000495846463c0080830000010000000000000000002303d3cbd3d6fe5c827c8a194844c252ac028c018080fa7070068074c5805000302e0004e9a00588b20bf251238e45283df8bb7445624de79229f156dc035d1f415a7b9333fc73345b64093934ebf99c415a7b9333fc7334df0bcfe5dcbf411926ec31e21e89c8b13a17804842663a95f4fedacc");
+        static void TestJunk()
+        {
+            //Logger.Write("Regular Log here");
+            //Logger.Info("Cool information here");
+            //Logger.Success("Successfully activated such and such");
+            //Logger.Warning("Cool warning here");
+            //Logger.Error("Oh No! An {0} occurred!", new[] { "error" });
 
-            Toolbelt.Utility.TestBitStream(responseData);
+            //ThreadPool.GetMaxThreads(out int workerThreadsCount, out int ioThreadsCount);
 
-            Console.WriteLine("Hello World!");
-            ScriptManager scriptManager = new ScriptManager();
-            scriptManager.Test();
+            //byte[] responseData = Toolbelt.Utility.StringToByteArray("4c000000495846463c0080830000010000000000000000002303d3cbd3d6fe5c827c8a194844c252ac028c018080fa7070068074c5805000302e0004e9a00588b20bf251238e45283df8bb7445624de79229f156dc035d1f415a7b9333fc73345b64093934ebf99c415a7b9333fc7334df0bcfe5dcbf411926ec31e21e89c8b13a17804842663a95f4fedacc");
 
-            Vector3 vec = new Vector3(1.0f,2.0f,3.0f);
-            byte[] bytes = new byte[12];
-            
-            Player player = new Player();
-            player.baseStats.STR = 100;
-            player.baseStats.DEX = 100;
-            player.baseStats.INT = 100;
-            player.baseStats.MND = 100;
-            player.baseStats.AGI = 100;
-            player.baseStats.VIT = 100;
-            player.baseStats.CHR = 100;
-            
-            //player.animation.id = 1;
-            //player.animation.subId = 0;
+            //Toolbelt.Utility.TestBitStream(responseData);
 
-            player.baseInfo.id = 449586;
-            player.baseInfo.targetId = 1408;
-            player.baseInfo.updateMask = UPDATETYPE.MOVEMENT|UPDATETYPE.DISPLAY;
+            //Console.WriteLine("Hello World!");
+            //ScriptManager scriptManager = new ScriptManager();
+            //scriptManager.Test();
 
-            player.moveInfo.rotation = 65;
-            player.moveInfo.position.x = 483.16f;
-            player.moveInfo.position.y = -8.0f;
-            player.moveInfo.position.z = 239.08f;
+            //Vector3 vec = new Vector3(1.0f,2.0f,3.0f);
+            //byte[] bytes = new byte[12];
 
-            player.moveInfo.moveCount = 2;
+            //Player player = new Player();
+            //player.baseStats.STR = 100;
+            //player.baseStats.DEX = 100;
+            //player.baseStats.INT = 100;
+            //player.baseStats.MND = 100;
+            //player.baseStats.AGI = 100;
+            //player.baseStats.VIT = 100;
+            //player.baseStats.CHR = 100;
 
-            player.moveInfo.targetIndex = 52;
+            ////player.animation.id = 1;
+            ////player.animation.subId = 0;
 
-            player.moveInfo.movementSpeed = 40;
-            player.moveInfo.animationSpeed = 40;
+            //player.baseInfo.id = 449586;
+            //player.baseInfo.targetId = 1408;
+            //player.baseInfo.updateMask = UPDATETYPE.MOVEMENT|UPDATETYPE.DISPLAY;
 
-            player.displayInfo.hpPercent = 64;
+            //player.moveInfo.rotation = 65;
+            //player.moveInfo.position.x = 483.16f;
+            //player.moveInfo.position.y = -8.0f;
+            //player.moveInfo.position.z = 239.08f;
 
-            player.displayInfo.animation = 1;
+            //player.moveInfo.moveCount = 2;
 
-            player.status = Data.Game.ENTITYSTATUS.NORMAL;
-            
-            player.nameFlags.flags = 0x00004000;
+            //player.moveInfo.targetIndex = 52;
 
-            player.look.size = 0;
-            player.look.model.face = 0x00;
-            player.look.model.race = (byte)RACE.ELVAAN_M;
-            player.look.head = 4208;
-            player.look.body = 8501;
-            player.look.hands = 12359;
-            player.look.legs = 16485;
-            player.look.feet = 20643;
-            player.look.main = 24865;
-            player.look.sub = 28672;
-            player.look.ranged = 32768;
+            //player.moveInfo.movementSpeed = 40;
+            //player.moveInfo.animationSpeed = 40;
 
-            player.name = "Tristia";
+            //player.displayInfo.hpPercent = 64;
 
-            player.linkshell = new Data.Game.Linkshell();
-            player.linkshell.color.Red = 173;
-            player.linkshell.color.Green = 225;
-            player.linkshell.color.Blue = 69;
+            //player.displayInfo.animation = 1;
 
-            PlayerUpdate playerUpdate = new PlayerUpdate(player, 2958);
-            
-            byte[] data = playerUpdate.data.Get();
+            //player.status = Data.Game.ENTITYSTATUS.NORMAL;
+
+            //player.nameFlags.flags = 0x00004000;
+
+            //player.look.size = 0;
+            //player.look.model.face = 0x00;
+            //player.look.model.race = (byte)RACE.ELVAAN_M;
+            //player.look.head = 4208;
+            //player.look.body = 8501;
+            //player.look.hands = 12359;
+            //player.look.legs = 16485;
+            //player.look.feet = 20643;
+            //player.look.main = 24865;
+            //player.look.sub = 28672;
+            //player.look.ranged = 32768;
+
+            //player.name = "Tristia";
+
+            //player.linkshell = new Data.Game.Linkshell();
+            //player.linkshell.color.Red = 173;
+            //player.linkshell.color.Green = 225;
+            //player.linkshell.color.Blue = 69;
+
+            //PlayerUpdate playerUpdate = new PlayerUpdate(player, 2958);
+
+            //byte[] data = playerUpdate.data.Get();
 
             //var builder = new FlatBuffers.FlatBufferBuilder(1024);
             //var name = builder.CreateString("Setzor");
@@ -111,9 +159,8 @@ namespace ZoneServer
 
             //var bytes = new FlatBuffers.ByteBuffer(buf);
             //var myPlayer = FBPlayer.GetRootAsFBPlayer(bytes);
-            Console.WriteLine(Toolbelt.Utility.ByteArrayToString(data));
-            Console.ReadKey();
-
+            //Console.WriteLine(Toolbelt.Utility.ByteArrayToString(data));
         }
+
     }
 }
